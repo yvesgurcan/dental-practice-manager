20b2f68df5e8f6c3685c903f3a96e8e7b5898790
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-02-01 19:06:55 -0800

    Update Billing.js

--- src/Pages/Billing.js
+++ src/Pages/Billing.js
@@ -1,1 +1,17 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import mapStateToProps from './../Store/mapStateToProps'
+import PageWrapper from './../Components/PageWrapper'
 
+class BillingComponent extends Component {
+  render () {
+    return (
+      <PageWrapper route="/billing" menuRoute="/billing">
+        content b
+      </PageWrapper>
+    )  
+  }
+}
+const Billing = connect(mapStateToProps)(BillingComponent)
+
+export default Billing
