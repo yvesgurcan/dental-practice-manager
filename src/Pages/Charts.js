02cec516a5e451d8f9f3bd23e5f6aef6c797ae35
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-02-01 19:12:42 -0800

    Update Charts.js

--- src/Pages/Charts.js
+++ src/Pages/Charts.js
@@ -1,1 +1,18 @@
 
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import mapStateToProps from './../Store/mapStateToProps'
+import PageWrapper from './../Components/PageWrapper'
+
+class ChartsComponent extends Component {
+  render () {
+    return (
+      <PageWrapper route="/charts" menuRoute="/charts">
+        content of charts
+      </PageWrapper>
+    )  
+  }
+}
+const Charts = connect(mapStateToProps)(ChartsComponent)
+
+export default Charts
