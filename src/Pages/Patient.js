890c06e510397ea4f1ac5389773e713610a40386
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-02-17 20:59:40 -0800

    Update Patient.js

--- src/Pages/Patient.js
+++ src/Pages/Patient.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import mapStateToProps from './../Store/mapStateToProps'
 import PageWrapper from './../Components/PageWrapper'
+import PatientBody from './../Components/Bodies/PatientBody'
 
 class PatientComponent extends Component {
   componentWillMount () {
-    this.props.dispatch({type: "STORE_ROUTE", ...this.props.match})
+    this.props.dispatch({type: 'STORE_ROUTE', ...this.props.match})
   }
   render () {
     return (
       <PageWrapper menuRoute='/patients'>
+        <PatientBody />
       </PageWrapper>
     )  
   }
