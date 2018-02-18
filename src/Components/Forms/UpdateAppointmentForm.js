75102997ee713d37e077a691505676aa377fcde8
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-02-17 21:49:41 -0800

    Update UpdateAppointmentForm.js

--- src/Components/Forms/UpdateAppointmentForm.js
+++ src/Components/Forms/UpdateAppointmentForm.js
@@ -1,1 +1,38 @@
 
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import mapStateToProps from './../../Store/mapStateToProps'
+import apiRequestHandler from './../../Utility/apiRequestHandler'
+import Block from './../Web/Block'
+import Button from './../Web/Button'
+import FormGroup from './../Web/Input/FormGroup'
+import Feedback from './../Feedback'
+
+class UpdateAppointmentFormComponent extends Component {
+  updatePatient = () => {
+    
+  }
+  
+  render () {
+    const { styles } = this.props.environment || {}
+    const { updateAppointmentFeedback } = this.props.appointments || {}
+    const [ appointments ] = this.props.appointments || {}
+    const updateAppointment = {}
+    return (
+      <Block style={styles.formWrapper}>
+        <Block>
+          <FormGroup
+            label='Appointment Date'
+            name='date'
+            value={(updateAppointment || {}).date}
+            onChange={this.updateAppointment}
+          />
+          <Feedback feedback={(updateAppointmentFeedback || {}).form} />
+        </Block>
+      </Block>
+    )
+  }
+}
+const UpdateAppointmentForm = connect(mapStateToProps)(UpdateAppointmentFormComponent)
+
+export default UpdateAppointmentForm
