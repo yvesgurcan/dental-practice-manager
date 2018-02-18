3a53d08d466e4bb279d38f906486a08f315fe24f
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-02-17 21:44:48 -0800

    Update UpdatePatientForm.js

--- src/Components/Forms/UpdatePatientForm.js
+++ src/Components/Forms/UpdatePatientForm.js
@@ -1,1 +1,38 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import mapStateToProps from './../../Store/mapStateToProps'
+import apiRequestHandler from './../../Utility/apiRequestHandler'
+import Block from './../Web/Block'
+import Button from './../Web/Button'
+import FormGroup from './../Web/Input/FormGroup'
+import Feedback from './../Feedback'
 
+class UpdatePatientFormComponent extends Component {
+  updatePatient = () => {
+    
+  }
+  
+  render () {
+    const { styles } = this.props.environment || {}
+    const { updatePatientFeedback } = this.props.patients || {}
+    const [ patients ] = this.props.patients || {}
+    const { 
+     updatePatient = {}
+    return (
+      <Block style={styles.formWrapper}>
+        <Block>
+          <FormGroup
+            label='First Name'
+            name='firstName'
+            value={(updatePatient || {}).firstName}
+            onChange={this.updatePatient}
+          />
+          <Feedback feedback={(updatePatientFeedback || {}).form} />
+        </Block>
+      </Block>
+    )
+  }
+}
+const UpdatePatientForm = connect(mapStateToProps)(UpdatePatientFormComponent)
+
+export default UpdatePatientForm
