fbc2ffc74a7e7daa366c4d1e7d0adb6e02d0c62d
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-02-18 16:12:34 -0800

    Update Operatories.js

--- src/Pages/Settings/Operatories.js
+++ src/Pages/Settings/Operatories.js
@@ -1,1 +1,21 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import mapStateToProps from './../../Store/mapStateToProps'
+import PageWrapper from './../../Components/PageWrapper'
+import OperatoriesBody from './../../Components/Bodies/OperatoriesBody'
 
+class OperatoriesComponent extends Component {
+  componentWillMount () {
+    this.props.dispatch({type: "STORE_ROUTE", ...this.props.match})
+  }
+  render () {
+    return (
+      <PageWrapper menuRoute='/settings'>
+        <OperatoriesBody />
+      </PageWrapper>
+    )  
+  }
+}
+const Operatories = connect(mapStateToProps)(OperatoriesComponent)
+
+export default Operatories
